b391ef6ea85c9558c0786af3815e4b14ee927629
CyanoFresh <cyanofresh@gmail.com>
2021-08-17 00:19:41 +0300

    test 2

--- app1/index.js
+++ app1/index.js
@@ -3,7 +3,7 @@ const fastify = require('fastify')({
 })
 
 fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
+    return { hello: 'world test' }
 })
 
 const start = async () => {
